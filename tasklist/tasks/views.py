491995e4b051e55143c9637e6faaf369557d5070
Brook Riggio <brook@rigg.io>
2017-06-30 12:37:36 -0700

    Adds home view

--- tasklist/tasks/views.py
+++ tasklist/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    incomplete = Task.incomplete_tasks.all()
+    complete = Task.objects.all().filter(complete=True)
+    context = {'incomplete': incomplete, 'complete': complete}
+    return render(request, "tasks/home.html", context=context)
